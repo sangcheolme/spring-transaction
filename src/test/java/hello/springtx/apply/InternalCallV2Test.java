0327476bc166e4a139e8b878a3d28691893779d2
SangCheol <scpark0698@gmail.com>
2023-10-03 00:22:06 +0900

    add InternalCallV2Test

--- src/test/java/hello/springtx/apply/InternalCallV2Test.java
+++ src/test/java/hello/springtx/apply/InternalCallV2Test.java
@@ -1,5 +1,6 @@
 package hello.springtx.apply;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,43 +12,51 @@ import org.springframework.transaction.support.TransactionSynchronizationManager
 
 @Slf4j
 @SpringBootTest
-public class InternalCallV1Test {
+public class InternalCallV2Test {
 
     @Autowired
-    private CallService callService;
+    CallService callService;
 
     @Test
-    void printProxy() {
-        log.info("callService class={}", callService.getClass());
-    }
-
-    @Test
-    void internalCall() {
-        callService.internal();
-    }
-
-    @Test
-    void externalCall() {
+    void externalCallV2() {
         callService.external();
     }
 
     @TestConfiguration
-    static class InternalCallV1Config {
+    static class InternalCallV2Config {
 
         @Bean
         public CallService callService() {
-            return new CallService();
+            return new CallService(internalService());
+        }
+
+        @Bean
+        public InternalService internalService() {
+            return new InternalService();
         }
     }
 
     @Slf4j
+    @RequiredArgsConstructor
     static class CallService {
+
+        private final InternalService internalService;
 
         public void external() {
             log.info("call external");
             printTxInfo();
-            internal();
+            internalService.internal();
         }
+
+        private void printTxInfo() {
+            boolean txActive
+                    = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("tx active={}", txActive);
+        }
+    }
+
+    @Slf4j
+    static class InternalService {
 
         @Transactional
         public void internal() {
@@ -61,5 +70,4 @@ public class InternalCallV1Test {
             log.info("tx active={}", txActive);
         }
     }
-
 }
